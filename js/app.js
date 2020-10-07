bd056866abee649124539a1e6f8e6df59d83fda4
Juan <kaenel10@gmail.com>
2020-10-07 10:36:48 -0300

    Leer el valor de la marca del automovil

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 //------------ Variables ------------
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
 
-//fechas
+const resultado = document.querySelector('#resultado'); //Contenedor para los resultados
+
+
+//Fechas
 const max = new Date().getFullYear(); //año actual
 const min = max - 10 //año minimo
+
+//Genera un objeto con datos de la búsqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
 
 //------------ Eventos ------------
 document.addEventListener('DOMContentLoaded',()=>{
@@ -14,7 +32,35 @@ document.addEventListener('DOMContentLoaded',()=>{
     llenarSelect();
 });
 
+//Event listeners para los selects de búsqueda
+marca.addEventListener('change',e=>{
+    datosBusqueda.marca = e.target.value;
+});
 
+year.addEventListener('change',e=>{
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change',e=>{
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change',e=>{
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change',e=>{
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change',e=>{
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change',e=>{
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+});
 //------------ Funciones ------------
 function mostrarAutos(){
     autos.forEach(auto=>{
